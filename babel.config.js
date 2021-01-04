61d0e9428ccf78661f9eb2d13ae6669668733966
monthem <lpoeh01@gmail.com>
2021-01-04 16:35:45 +0900

    COMPILE: added absolute path alias for src subdirectories

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    ["module-resolver", {
+      root: ["."],
+      extensions: [".js", ".jsx", ".ts", ".tsx", ".ios.js", ".android.js", ".json"],
+      alias: {
+        "src": "./src",
+        "@components": "./src/components",
+        "@redux": "./src/redux",
+        "@router": "./src/router",
+        "@screens": "./src/screens",
+        "@utils": "./src/utils",
+        "@types": "./src/types",
+      }
+    }]
+  ]
 };
