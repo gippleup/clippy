a7c1b9f743d764573c91d79bee80e93c2b9a5219
monthem <lpoeh01@gmail.com>
2021-01-05 12:27:22 +0900

    ADD: added reduxHooks for each reducer

---
+++ src/hooks/useReduxSearchResult.ts
@@ -0,0 +1,24 @@
+import { ReduxRootState } from "@redux/schema";
+import { useDispatch, useSelector } from "react-redux";
+import searchResultActions from '@redux/actions/searchResult';
+import { QueryStateStatus } from "@redux/reducers/query";
+import { ReduxHookMethod } from "./schema";
+
+const selectSearchResult = (state: ReduxRootState) => state.searchResult;
+
+const useQuery = () => {
+  const searchResultState = useSelector(selectSearchResult);
+  const dispatch = useDispatch();
+  const methods: ReduxHookMethod<typeof searchResultActions> = {
+    clip: (indicator) => dispatch(searchResultActions.clip(indicator)),
+    push: (searchResult) => dispatch(searchResultActions.push(searchResult)),
+    set: (searchResults) => dispatch(searchResultActions.set(searchResults)),
+    unclip: (indicator) => dispatch(searchResultActions.unclip(indicator)),
+  }
+  return {
+    state: searchResultState,
+    methods,
+  };
+}
+
+export default useQuery;
