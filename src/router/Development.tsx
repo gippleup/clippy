ca0ed388aa8ab6bc7a200daa124b612b28d30800
monthem <lpoeh01@gmail.com>
2021-01-05 12:39:31 +0900

    INIT: initialized dev route for SearchBar

--- src/router/Development.tsx
+++ src/router/Development.tsx
@@ -1,11 +1,10 @@
-import Developer from '@screens/Developer';
-import ButtonTester from '@screens/Testers/ButtonTester';
+import SearchBarTester from '@screens/Tester/SearchBarTester';
 import { defineCustomRoute } from '@utils/router';
 
 const routes = {
-  ButtonTester: defineCustomRoute({
-    devName: "버튼 테스터",
-    component: ButtonTester,
+  SearchBarTester: defineCustomRoute({
+    devName: "서치바 테스터",
+    component: SearchBarTester,
     params: undefined,
   }),
 }
