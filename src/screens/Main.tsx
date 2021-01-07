2f6ef332a37aee5dea5fb9fb8edb33dcb25c57ce
monthem <lpoeh01@gmail.com>
2021-01-07 10:07:23 +0900

    MODIFY: replaced bottom-tab-navigation on Main with material-top-tap-navigation for swipe

--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -1,15 +1,14 @@
 import React, { Component } from 'react'
 import { StatusBar, ViewStyle } from 'react-native'
-import {BottomTabBarOptions, createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import SearchArticle from './SearchArticle';
 import ClippedArticle from './ClippedArticle';
 import { getIcon } from '@api/icons';
 import useReduxTheme from '@hooks/useReduxTheme';
 import chroma from 'chroma-js';
 import { getColorTheme } from '@api/colortheme';
+import { createMaterialTopTabNavigator, MaterialTopTabBarOptions } from '@react-navigation/material-top-tabs';
 
-
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialTopTabNavigator();
 
 const TabNavigator = () => {
   const {state: {name}} = useReduxTheme();
@@ -19,24 +18,24 @@ const TabNavigator = () => {
     borderTopColor: chroma(background).brighten().hex(),
   }
 
-  const tabBarOptions: BottomTabBarOptions = {
+  const tabBarOptions: MaterialTopTabBarOptions = {
     style: tabBarStyle,
   }
 
   return (
-    <Tab.Navigator tabBarOptions={tabBarOptions}>
+    <Tab.Navigator tabBarPosition="bottom" tabBarOptions={tabBarOptions}>
       <Tab.Screen
           name="Clipped"
           component={ClippedArticle}
           options={{
-            tabBarIcon: ({color, size}) => getIcon("FontAwesome",{color, size, name: "bookmark"}),
+            tabBarIcon: ({color, focused}) => getIcon("FontAwesome",{color, size: 30, name: "bookmark"}),
           }}
         />
         <Tab.Screen
           name="Search"
           component={SearchArticle}
           options={{
-            tabBarIcon: ({color, size}) => getIcon("FontAwesome",{color, size, name: "search"})
+            tabBarIcon: ({color, focused}) => getIcon("FontAwesome",{color, size: 30, name: "search"})
           }}
         />
     </Tab.Navigator>
